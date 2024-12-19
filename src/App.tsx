de9c82fb3cd1fed669a24c84730844754862f059
Minyoung Kim <112898937+minyouki@users.noreply.github.com>
2024-12-19 13:22:42 +0900

    feat: 행사 메인 페이지 퍼블리싱 (#13)

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,36 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { reset } from './styles/global';
 import theme from './styles/theme';
+import EventDetailPage from './views/EventDetail/pages/EventDetailPage';
 import EventGeneratePage from './views/EventGenerate/page/EventGeneratePage';
+import EventMainPage from './views/EventMain/pages/EventMainPage';
 import MyPage from './views/My/pages/MyPage';
 import Ticket from './views/My/pages/Ticket';
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/', element: <MyPage/>
-    }, {
-      path   : '/make-event', 
-      element: <EventGeneratePage/>,
-    }, {
-      path   : '/ticket', 
-      element: <Ticket />,
-    }
+      path   : '/',
+      element: <EventMainPage />,
+    },
+    {
+      path    : '/my',
+      element : <MyPage />,
+      children: [
+        {
+          path   : 'ticket',
+          element: <Ticket />,
+        },
+      ]
+    },
+    {
+      path   : '/make-event',
+      element: <EventGeneratePage />,
+    },
+    {
+      path   : '/event/:id',
+      element: <EventDetailPage />,
+    },
   ]);
 
   return (
---
+++ src/views/My/type/myType.ts
@@ -0,0 +1,3 @@
+export interface OutletContext {
+  setCurrentTap: React.Dispatch<React.SetStateAction<string>>;
+}
